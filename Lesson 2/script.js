e36830d37349acb3a3c893fefee5dc160c3bc7a9
KarenWo <lajannetwork@gmail.com>
2024-02-10 19:34:23 +0200

    attempt_1

---
+++ Lesson 2/script.js
@@ -0,0 +1,15 @@
+let years = prompt("Введіть, будь ласка, скільки вам років");
+
+if (!isNaN(years) && years >= 0 && years <= 100) {
+    
+    if (years === 1 || years % 10 === 1) {
+        console.log(years + ' рік');
+    } else if (years % 10 >= 2 && years % 10 <= 4) {
+        console.log(years + ' роки');
+    } else {
+        console.log(years + ' років');
+    }
+
+} else {
+    console.log("Введіть, будь ласка, додатнє число");
+}
