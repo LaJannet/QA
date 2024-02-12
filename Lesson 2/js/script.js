cb869d04930bdf1ad2b2f2fb7aea3d574dfb676f
KarenWo <lajannetwork@gmail.com>
2024-02-12 21:07:39 +0200

    try_1

--- Lesson 2/js/script.js
+++ Lesson 2/js/script.js
@@ -1,7 +1,9 @@
 let years = prompt("Введіть, будь ласка, скільки вам років");
-
-if (!isNaN(years) && years >= 0 && years <= 100) {
-    
+if (typeof years == "object"){ 
+    console.log("Ви натиснули кнопку \"Відміна\".\nОновіть, будь ласка, сторінку");
+} else if (isNaN(years) || years < 0 || years > 100){ 
+    console.log("Неправильно введені дані.\nВведіть, будь ласка, додатнє число від 0 до 100 включно");
+} else {
     if (years == 1 || (years >= 21 && years % 10 == 1)) {
         console.log(years + ' рік');
     } else if (years >= 2 && years <= 4 || (years >= 22 && years % 10 >= 2 && years % 10 <= 4)) {
@@ -9,7 +11,4 @@ if (!isNaN(years) && years >= 0 && years <= 100) {
     } else {
         console.log(years + ' років');
     }
-
-} else {
-    console.log("Введіть, будь ласка, додатнє число");
 }
