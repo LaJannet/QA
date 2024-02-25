bc2b97a5d31b5fb191773dc26b11a6e7a33bfb6b
KarenWo <lajannetwork@gmail.com>
2024-02-25 13:44:03 +0200

    try1

---
+++ Lesson 7/script.js
@@ -0,0 +1,25 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov.sfd@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+var re = /([a-zA-Z0-9])(@gmail|@yahoo).com$/
+var res = arr.filter(item => re.test(item.email));
+
+console.log("The main array =");
+console.log(arr);
+console.log("\nConverted array =");
+console.log(res);
